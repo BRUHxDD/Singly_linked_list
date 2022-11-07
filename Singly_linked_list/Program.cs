b3322908e1389b6ed9bd88a4e9bf2a702fcbc95a
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-11-07 17:04:56 +0700

    add "addNote"

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -1,1 +1,44 @@
 ﻿
+
+
+
+
+
+namespace single_linked_list
+{
+    // each node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        public void addNote() // Add a node in the list
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            if(START = null || rollNo <= START.rollNumber)
+            {
+                if((START != null) &&(rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+            }
+        }
+    }
+}
